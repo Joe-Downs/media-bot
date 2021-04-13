ecfb6dd92b6771549f1af9801406e84c1dfb86ba
Joe Downs <joedowns502@gmail.com>
2021-04-13 15:14:59 -0400

    mediaBot.py: add basic bot code borrowed from other bots
    + sudo [command]
      + stop

---
+++ mediaBot.py
@@ -0,0 +1,22 @@
+# Python Modules
+import sys
+# Third-Party Modules
+import discord
+from discord.ext import commands
+# Custom Modules
+import config
+
+owner_ID = 174362561385332736
+botToken = config.getToken()
+prefix = config.getPrefix()
+
+bot = commands.Bot(command_prefix = prefix)
+
+@bot.command()
+async def sudo(ctx, arg):
+    if arg == "exit" or arg == "stop":
+        await ctx.send("Sleep mode activated...")
+        await bot.close()
+        sys.exit()
+
+bot.run(botToken)
