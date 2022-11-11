cc21381d21e8efe303b71a9b552bb0f9b6ea4229
Joe Downs <joe@dwns.dev>
2022-11-11 00:52:07 -0500

    add configurable database name
    Now the user can select a name for their database to be reused each time. If a
    name is not provided, then a new database will be created each time with a
    timestamp. The user does not have to specify the .db extension, but they may.

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 
+def getDatabaseName():
+    dbName = config["Bot"]["DatabaseName"]
+    return dbName
+
 def getLogLevel():
     logLevel = config["General"]["LogLevel"].upper()
     return logLevel
