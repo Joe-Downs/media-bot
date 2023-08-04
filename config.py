b97d1cd94fff2ce9b8bedd81413551fc0523d8f1
Joe Downs <joe@dwns.dev>
2023-08-04 11:20:47 -0400

    move database-related logic to config script for reuse

--- config.py
+++ config.py
@@ -3,9 +3,22 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 
+# Get the custom database name from the config file, if there is one. If there
+# is, always use that. If not, create a new database every time the bot starts
+# up, giving it a timestamp.
 def getDatabaseName():
+    # Strip the extension if there is one, we'll add one. Since the user might
+    # configure their filename without it.
     dbName = config["Bot"]["DatabaseName"]
-    return dbName
+    if len(dbName) == 0:
+        nowDatetime = datetime.now()
+        timestamp = nowDatetime.strftime("%y%m%d-%H%M%S")
+        dbName = f"media-{timestamp}"
+    else:
+        # Strip the extension if there is one, we'll add one. Since the user
+        # might configure their filename without it.
+        dbName = dbName.rstrip(".db")
+    return f"{dbName}.db"
 
 def getLogLevel():
     logLevel = config["General"]["LogLevel"].upper()
--- database.py
+++ database.py
@@ -7,21 +7,9 @@ import config
 # This file ensures that the database is created (or already exists) with all
 # the necessary tables
 
-# Get the custom database name from the config file, if there is one. If there
-# is, always use that. If not, create a new database every time the bot starts
-# up, giving it a timestamp.
 dbName = config.getDatabaseName()
 
-if len(dbName) == 0:
-    nowDatetime = datetime.now()
-    timestamp = nowDatetime.strftime("%y%m%d-%H%M%S")
-    dbFilename = f"media-{timestamp}"
-else:
-    # Strip the extension if there is one, we'll add one. Since the user might
-    # configure their filename without it.
-    dbFilename = dbName.rstrip(".db")
-
-conn = sqlite3.connect(f"{dbFilename}.db")
+conn = sqlite3.connect(dbName)
 # The Row instance allows for the row returned by sqlite3 to be mapped by column
 # name and index in a dictionary-like format. Additionally, "it
 # supports...iteration, representation, equality testing and len()"
